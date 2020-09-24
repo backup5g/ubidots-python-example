49bb046b2cf44f4a68d60cfadece8e1b3527e66c
Diego Eiras <eiras.lucio@gmail.com>
2020-09-23 23:31:45 -0300

    delete env file and add env-example

---
+++ env-example.py
@@ -0,0 +1,7 @@
+BROKER_ENDPOINT = 'industrial.api.ubidots.com'
+PORT = 1883
+MQTT_USERNAME = '...' # Coloque o seu token aqui
+MQTT_PASSWORD = ''
+TOPIC = '/v1.6/devices/'
+DEVICE_LABEL = '' # Escolha o nome do seu dispositivo
+VARIABLE_LABELS = ['', '', '', ''] # Insira o nome das variáveis
